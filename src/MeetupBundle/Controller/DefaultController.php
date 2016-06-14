117ad4c04d2c99d6670e63665a083230f6a4955d
Cédric Oloa <cedric.oloa@laposte.net>
2016-06-14 12:04:07 +0200

    reparer

--- src/MeetupBundle/Controller/DefaultController.php
+++ src/MeetupBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace MeetupBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -11,7 +12,7 @@ class DefaultController extends Controller
      * @Route("/")
      */
      public function dashboardAction(Request $request){
-         $json_string = file_get_contents("api.meetup.com/2/groups?&sign=true&photo-host=public&category_id=34&country=france&key=17662761a2d418394102b53502864");
+         $json_string = file_get_contents("https://api.meetup.com/2/groups?&sign=true&photo-host=public&category_id=34&country=fr&city=paris&key=17662761a2d418394102b53502864");
             $jsonData = json_decode($json_string, true);
             var_dump($jsonData); exit;
      }
